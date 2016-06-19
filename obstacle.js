ac8bfc8417f323cea020b02b7fb4d2bf4e199fa1
SzuYaoJen <tim840725@gmail.com>
2016-06-19 21:30:37 +0800

    two kinds of obstacle & soundbanks Signed-off-by: SzuYaoJen <tim840725@gmail.com>

--- obstacle.js
+++ obstacle.js
@@ -1,10 +1,11 @@
+const far = 2000;
 function Obstacle(scene,camera){
 	this.height = 30; //物體高度
 	this.speed = 10; //物體速度
 	this.dir = [0,0,-1];
 	this.scene = scene;
 	this.cam = camera;
-	this.position = [0,0,900];
+	this.position = [0,0,-50];
 	this.mesh;
 	this.obj;
 	this.block = [false,false,false,false,false,false,false,false,false];
@@ -37,7 +38,7 @@ Obstacle.prototype.collision = function(){
 		this.effect();
 	}else if(this.cam.position.x == 0 && this.cam.position.y == -40 && this.block[7]){
 		this.effect();
-	}else if(this.cam.position.x == -40 && this.cam.position.y == -40 && this.block[8]){
+	}else if(this.cam.position.x == 40 && this.cam.position.y == -40 && this.block[8]){
 		this.effect();
 	}
 	
@@ -49,10 +50,9 @@ Obstacle.prototype.effect = function(){
 //只佔一個格子的物件
 function UnitObstacle(num,scene,camera){
 	Obstacle.call(this,scene,camera);
-	this.scene = scene;
 	this.setBlock(num);
 	this.draw();
-	this.setPosition(num);
+	//this.setPosition(num);
 }
 UnitObstacle.prototype = new Obstacle(); //繼承
 UnitObstacle.prototype.constructor = UnitObstacle; //確立建構者
@@ -65,31 +65,31 @@ UnitObstacle.prototype.setPosition = function(num){
 	this.setBlock(num);
 	switch(num){
 		case 0:
-			this.position = [-40,40,900];
+			this.position = [-40,40,far];
 		break;
 		case 1:
-			this.position = [0,40,900];
+			this.position = [0,40,far];
 		break;
 		case 2:
-			this.position = [40,40,900];
+			this.position = [40,40,far];
 		break;
 		case 3:
-			this.position = [-40,0,900];
+			this.position = [-40,0,far];
 		break;
 		case 4:
-			this.position = [0,0,900];
+			this.position = [0,0,far];
 		break;
 		case 5:
-			this.position = [40,0,900];
+			this.position = [40,0,far];
 		break;
 		case 6:
-			this.position = [-40,-40,900];
+			this.position = [-40,-40,far];
 		break;
 		case 7:
-			this.position = [0,-40,900];
+			this.position = [0,-40,far];
 		break;
 		case 8:
-			this.position = [-40,-40,900];
+			this.position = [-40,-40,far];
 		break;
 	}
 	this.mesh.position = new BABYLON.Vector3(this.position[0], this.position[1], this.position[2]);
@@ -104,50 +104,103 @@ UnitObstacle.prototype.draw = function(){
 	this.mesh.position = new BABYLON.Vector3(this.position[0], this.position[1], this.position[2]);
 	
 }	
-function RodObstacle(num){
-	Obstacle.call(this);
+//棒狀障礙物
+function RodObstacle(num,scene,camera){
+	Obstacle.call(this,scene,camera);
 	this.setBlock(num);
+	this.draw(num);
+	//this.setPosition(num);
 }
 RodObstacle.prototype = new Obstacle(); //繼承
 RodObstacle.prototype.constructor = RodObstacle; //確立建構者
-RodObstacle.prototype.setBlock = function(num){
+
+RodObstacle.prototype.draw = function(num){
+	var materialRod = new BABYLON.StandardMaterial("rod", this.scene);
+	materialRod.diffuseColor = new BABYLON.Color3(0.1, 0.1, 0.9);//blue
+	materialRod.specularColor = new BABYLON.Color3(0.0, 0.0, 0.0);// 0
+	this.mesh = new BABYLON.Mesh.CreateBox("rod",30,this.scene);
+	this.mesh.material = materialRod;
+	this.mesh.position = new BABYLON.Vector3(this.position[0], this.position[1], this.position[2]);
+	
+}	
+RodObstacle.prototype.setPosition = function(num){
+	this.mesh.scaling = new BABYLON.Vector3(1,1,1);
+	this.mesh.rotation.z = 0;
+	this.setBlock(num);
 	switch(num){
 		case 0:
-			this.block[0] = true;
-			this.block[1] = true;
-			this.block[2] = true;
+			this.position = [0,40,far];
 		break;
 		case 1:
+		case 4:
+		case 6:
+		case 7:
+			this.position = [0,0,far];
+		break;
+		case 2:
+			this.position = [0,-40,far];
+		break;
+		case 3:
+			this.position = [-40,0,far];
+		break;
+		case 5:
+			this.position = [40,0,far];
+		break;
+
+
+	}
+	this.mesh.position = new BABYLON.Vector3(this.position[0], this.position[1], this.position[2]);
+	if(num<3){
+		this.mesh.scaling = new BABYLON.Vector3(6,0.5,0.5);
+	}else if(num<6){
+		this.mesh.scaling = new BABYLON.Vector3(0.5,6,0.5);
+	}else if(num==6){
+		this.mesh.scaling = new BABYLON.Vector3(6,0.5,0.5);
+		this.mesh.rotation.z = Math.PI/4*3;
+	}else if(num==7){
+		this.mesh.scaling = new BABYLON.Vector3(6,0.5,0.5);
+		this.mesh.rotation.z = Math.PI/4;
+	}
+}
+RodObstacle.prototype.setBlock = function(num){
+	this.block = [false,false,false,false,false,false,false,false,false];
+	switch(num){
+		case 0: //上一
+			this.block[0] = true;
+			this.block[1] = true;
+			this.block[2] = true;
+		break;
+		case 1: //中一
 			this.block[3] = true;
 			this.block[4] = true;
 			this.block[5] = true;
 		break;
-		case 2:
+		case 2: //下一
 			this.block[6] = true;
 			this.block[7] = true;
 			this.block[8] = true;
 		break;
-		case 3:
+		case 3: //左 |
 			this.block[0] = true;
 			this.block[3] = true;
 			this.block[6] = true;
 		break;
-		case 4:
+		case 4: //中 |
 			this.block[1] = true;
 			this.block[4] = true;
 			this.block[7] = true;
 		break;
-		case 5:
+		case 5: //右 |
 			this.block[2] = true;
 			this.block[5] = true;
 			this.block[8] = true;
 		break;
-		case 6:
+		case 6: //左上斜到右下
 			this.block[0] = true;
 			this.block[4] = true;
 			this.block[8] = true;
 		break;
-		case 7:
+		case 7: //右上到左下
 			this.block[2] = true;
 			this.block[4] = true;
 			this.block[6] = true;
